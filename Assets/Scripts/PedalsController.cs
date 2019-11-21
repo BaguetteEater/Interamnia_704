d728a2fb6f471b2153a0bf13e89d72a804eec926
Arthur Martello <arthur.martello@etu.utc.fr>
2019-11-21 15:01:37 +0000

    Spaceship Controller

---
+++ Assets/Scripts/PedalsController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PedalsController : MonoBehaviour
+{
+    public GameObject spaceship;
+    private SpaceshipController spaceshipController;
+
+    void Start()
+    {
+        this.spaceshipController = spaceship.GetComponent<SpaceshipController>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    // Intensity should be between 0 and 1
+    private void OnRightPedalInput(int intensity)
+    {
+        // ToDo: Move the Spaceship
+    }
+
+
+    private void OnMiddlePedalInput(int intensivity)
+    {
+        // ToDo: Make the Spaceship brake
+    }
+}
---
+++ Assets/Scripts/SpaceshipController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceshipController : MonoBehaviour
+{
+    private Rigidbody rigidbody;
+
+    public uint forwardSpeed { get; set; }
+    public int verticalSpeed { get; set; }
+    public int horizontalSpeed { get; set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rigidbody = GetComponent<Rigidbody>();
+
+        // Spaceship is static on beginning
+        this.forwardSpeed = 0;
+        this.verticalSpeed = 0;
+        this.horizontalSpeed = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveForward();
+        MoveHorizontaly();
+        MoveVerticaly();
+    }
+
+    private void MoveForward()
+    {
+        this.rigidbody.AddForce(this.transform.forward * this.forwardSpeed);
+        // ToDo: Either put the camera into the Spaceship or move it independently
+        // ToDo: Play engine sounds
+    }
+    
+    private void MoveVerticaly()
+    {
+        this.rigidbody.AddTorque(this.transform.right * - this.verticalSpeed);
+    }
+
+    private void MoveHorizontaly()
+    {
+        this.rigidbody.AddTorque(this.transform.forward * - this.horizontalSpeed);
+    }
+}
