c7d4630d24047f82100a6a588b867c4cb6d7447c
Arthur Martello <arthur.martello@etu.utc.fr>
2019-12-08 13:41:58 +0000

    Sounds!

--- Assets/Scripts/SpaceshipInput.cs
+++ Assets/Scripts/SpaceshipInput.cs
@@ -1,8 +1,5 @@
 ﻿using UnityEngine;
 
-/// <summary>
-/// Class specifically to deal with input.
-/// </summary>
 public class SpaceshipInput : MonoBehaviour
 {
     [Space]
@@ -21,57 +18,30 @@ public class SpaceshipInput : MonoBehaviour
     // How quickly the throttle reacts to input.
     private const float THROTTLE_SPEED = 0.5f;
 
-    //To get the ship's velocity
-    private Rigidbody rbody;
+    private Rigidbody rigidbody;
+    private AudioSource motorSound;
 
-    // Keep a reference to the ship this is attached to just in case.
-    private Spaceship ship;
-
-	public GameObject joystick;
-
-    private void Awake()
+    private void Start()
     {
-        rbody = GetComponent<Rigidbody>();
-        ship = GetComponent<Spaceship>();
+        rigidbody = GetComponent<Rigidbody>();
+
+        motorSound = GetComponents<AudioSource>()[1];
+        motorSound.loop = true;
+        motorSound.Play();
     }
 
     private void Update()
     {
-		
-        // pitch = Input.GetAxis("Vertical");
-        // yaw = Input.GetAxis("Horizontal");
-
-		//pitch = joystickInput.getPitch();
-		//yaw = joystickInput.getYaw();
-
         strafe = 0.0f;
+        motorSound.pitch = rigidbody.velocity.z / 50;
     }
 
-    /// <summary>
-    /// Uses R and F to raise and lower the throttle.
-    /// </summary>
-    private void UpdateKeyboardThrottle(KeyCode increaseKey, KeyCode decreaseKey)
-    {
-
-        // When moving forward, velocity.z value is between [0;+infinite]
-
-        float target = throttle;
-
-        if (Input.GetKey(increaseKey))
-            target = 1.0f;
-        else if (Input.GetKey(decreaseKey) && this.rbody.velocity.z > 3) // On empeche de reculer si on recule deja ou à l'arret
-            target = -1.0f;
-
-
-        throttle = Mathf.MoveTowards(throttle, target, Time.deltaTime * THROTTLE_SPEED);
-    }
-
-	public void UpdateThrottle(float input) 
+    public void UpdateThrottle(float input) 
 	{
 		throttle = Mathf.MoveTowards(throttle, input, Time.deltaTime * THROTTLE_SPEED);
-	}
+    }
 
-	public void UpdatePitch(float input)
+    public void UpdatePitch(float input)
 	{
 		pitch = input;
 	}
